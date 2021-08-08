18f339512f38782b48e791330a858bf512c34cf1
McKenzie Postell <mckenziepostell99@gmail.com>
2021-08-08 17:27:38 -0500

    Uploading website

---
+++ src/containers/App.js
@@ -0,0 +1,51 @@
+import React, { component } from 'react';
+import Cardlist from '../components/Cardlist.js';
+import SearchBox from '../components/SearchBox.js';
+import Scroll from '../components/Scroll.js'
+import ErrorBoundary from '../components/ErrorBoundary.js';
+import './App.css';
+
+
+// eslint-disable-next-line
+class App extends React.Component {
+ constructor() {
+   super()	
+   this.state = {	
+    robots: [],
+    searchfield: ''
+  }
+}    
+
+componentDidMount() {
+  fetch('https://jsonplaceholder.typicode.com/users')
+   .then(response=> response.json())
+   .then(users => this.setState({ robots: users}));
+}
+
+ onSearchChange = (event) => {
+   this.setState({searchfield: event.target.value})
+}
+   
+
+   render (){
+     const { robots, searchfield} = this.state
+     const filteredRobots = robots.filter(robot =>{
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase())
+     })	
+     return !robots.length?
+        <h1>Loading</h1> :
+       (
+	        <div className ='tc'>
+		        <h1 className='f1'> RoboFriends </h1>
+	            <SearchBox searchChange={this.onSearchChange}/>
+                <Scroll>
+                 <ErrorBoundary>
+               <Cardlist robots={filteredRobots} />
+             </ErrorBoundary>
+           </Scroll>
+         </div>
+	    );
+    }
+  }  
+
+export default App; 
